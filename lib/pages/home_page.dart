f54bc71ddf13863b38c200bf9b15038980eb319f
iwen <iwenchaos@gmail.com>
2019-04-09 23:50:53 +0800

    项目首页home page的头部

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yyts/widgets/app_drawer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,10 +14,15 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    //这里需要进行网络获取数据
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white, body: new Text("哈哈哈"));
+    //这里需要构建整个页面ui
+    return Scaffold(
+        backgroundColor: Colors.white,
+        drawer: AppDrawer(),
+        body: new Text("哈哈哈"));
   }
 }
--- lib/redux/states/main.dart
+++ lib/redux/states/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_yyts/redux/states/home.dart';
+import 'package:flutter_yyts/redux/states/profile.dart';
 
 @immutable
 class ReduxState {
   final HomeState home;
+  final ProfileState profile;
 
-  const ReduxState({this.home});
+  const ReduxState({this.profile, this.home});
 }
