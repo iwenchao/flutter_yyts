1d0e0dba5c5c4c8ef6550214202ee90960618c93
iwen <iwenchaos@gmail.com>
2019-04-09 23:06:59 +0800

    项目首页tab的初稿,准备网络层

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int sliderIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.white, body: new Text("哈哈哈"));
