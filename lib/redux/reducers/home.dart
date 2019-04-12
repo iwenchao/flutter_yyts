bc7e47e89dbeeeab208c9586733a695a34d444f6
iwen <iwenchaos@gmail.com>
2019-04-12 23:59:13 +0800

    网络层api：banner

--- lib/redux/reducers/home.dart
+++ lib/redux/reducers/home.dart
@@ -4,10 +4,12 @@ import 'package:flutter_yyts/redux/states/home.dart';
 
 HomeState reducer(HomeState state, ActionType action) {
   if (action is UpdateTVSchedule) {
-    return state.copyWith(schedules: action.payload);
+    return state.copyWith(schedules: action.payload, isLoading: false);
   }
   if (action is UpdateAds) {
     return state.copyWith(ads: action.payload, isLoading: false);
+
+
   }
   return state;
 }
