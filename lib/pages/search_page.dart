7047f2e93b31a41f19845063212564d097222462
iwen <iwenchaos@gmail.com>
2019-04-18 22:56:15 +0800

    搜索页面，暂无实现

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
