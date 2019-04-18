42a4152945a76cceed1ed61b152c6bbea368c3f1
iwen <iwenchaos@gmail.com>
2019-04-18 22:56:55 +0800

    排行榜的rank grid组件

---
+++ lib/widgets/video_grid_item_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_yyts/models/resource_info.dart';
+import 'package:flutter_yyts/widgets/tag_widget.dart';
+
+class VideoGridItem extends StatelessWidget {
+  final Resource resource;
+
+  VideoGridItem({Key key, this.resource}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: null,
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Card(
+                elevation: 8,
+                child: AspectRatio(
+                  aspectRatio: 4 / 3,
+                  child: Hero(
+                    tag: resource.poster,
+                    child: FadeInImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(resource.poster ??
+                          "http://renren2.maoyun.tv/apptu/1554741804331_7765.jpg"),
+                      placeholder: AssetImage("assets/images/placeholder.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                resource.cnname,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Text(
+              resource.playStatus,
+              style: Theme.of(context).textTheme.body2,
+            ),
+            resource.channelCn != null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TagView(
+                      text: resource.channelCn,
+                      color: Theme.of(context).primaryColor,
+                      border: true,
+                    ),
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/video_grid_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_yyts/models/resource_info.dart';
+import 'package:flutter_yyts/widgets/video_grid_item_widget.dart';
+
+class VideoGridView extends StatelessWidget {
+  final List<Resource> resources;
+
+  VideoGridView({Key key, this.resources}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: GridView.builder(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: resources.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 9 / 18,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8),
+          itemBuilder: (context, index) {
+            return VideoGridItem(resource: resources[index]);
+          }),
+    );
+  }
+}
