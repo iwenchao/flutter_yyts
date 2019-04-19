0671bcbc8c4daf422b4a32bb5f427b1be0782ff3
iwen <iwenchaos@gmail.com>
2019-04-19 22:56:19 +0800

    排序控件

---
+++ lib/models/sort_info.dart
@@ -0,0 +1,16 @@
+
+class SortVo {
+	final String key;
+	final String text;
+	
+	SortVo({this.key, this.text});
+	
+	static List<SortVo> defaults = [
+		SortVo(key:"itemupdate", text:"按更新时间"),
+		SortVo(key: "views", text: "按发布日期"),
+		SortVo(key: "rank", text: "按排名"),
+		SortVo(key: "score", text: "按评分"),
+		SortVo(key: "dateline", text: "按点击率"),
+	]	;
+	
+}
---
+++ lib/widgets/sort_bar_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SortBarView extends StatefulWidget {
+  final Function callback;
+
+  SortBarView({Key key, this.callback}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SortBarViewState();
+  }
+  
+
+ 
+}
+
+class _SortBarViewState extends State<SortBarView>{
+	
+	
+	@override
+  void initState() {
+    super.initState();
+  }
+	
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+	
+}
