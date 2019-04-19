50fb2db928f912856e2700b8ab88c38d18881304
iwen <iwenchaos@gmail.com>
2019-04-19 23:07:46 +0800

    排序控件完成

--- lib/widgets/sort_bar_widget.dart
+++ lib/widgets/sort_bar_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yyts/models/sort_info.dart';
+import 'package:flutter_yyts/widgets/section_title_widget.dart';
 
 class SortBarView extends StatefulWidget {
   final Function callback;
@@ -9,22 +11,45 @@ class SortBarView extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _SortBarViewState();
   }
-  
-
- 
 }
 
-class _SortBarViewState extends State<SortBarView>{
-	
-	
-	@override
+class _SortBarViewState extends State<SortBarView> {
+  SortVo sortVo = SortVo.defaults.first;
+
+  @override
   void initState() {
     super.initState();
   }
-	
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Container(
+      child: PopupMenuButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SectionTitleView(
+              title: sortVo.text,
+            ),
+            Icon(Icons.sort_by_alpha)
+          ],
+        ),
+        onSelected: (SortVo vo) {
+          setState(() {
+            sortVo = vo;
+          });
+          widget.callback(vo);
+        },
+        itemBuilder: (context) {
+          return SortVo.defaults
+              .map((value) => PopupMenuItem(
+                    key: Key(value.key),
+                    value: value,
+                    child: Text(value.text),
+                  ))
+              .toList();
+        },
+      ),
+    );
   }
-	
 }
